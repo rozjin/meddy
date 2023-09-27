3a6bb8630ae9d6e0da8033005bf10a4cb12aedb9
racemus <yavuz.rao@gmail.com>
2023-09-27 00:02:14 -0400

    head file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
+'use client'
+
 import Providers from "@/meddy/app/providers"
 import "@/meddy/app/globals.css"
 import Toaster from "@/meddy/components/Toaster"
 
-export const metadata = {
-  title: 'Meddy',
-  description: 'NZ\'s digital pharmacy',
-}
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light">
+      <head>
+        <title>Meddy</title>
+      </head>
       <body> 
         <Providers>
           <Toaster />
