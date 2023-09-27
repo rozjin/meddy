a99f2268a5e4b29837d8f80073fa909d5907e628
racemus <yavuz.rao@gmail.com>
2023-09-26 23:54:15 -0400

    aws key

--- app/api/s3.ts
+++ app/api/s3.ts
@@ -5,6 +5,6 @@ export const s3 = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY as string,
-        secretAccessKey: process.env.AWS_SECRET_KEY as string
+        secretAccessKey: process.env.AWS_KEY as string
     }
 });
